cd8cb91ffe1b7aa0a1b5504347272e74b4c096d5
Nathan Crock <ndc08@spear-5.rcc.fsu.edu>
2016-02-07 03:15:51 -0500

    Added Webcam to the Downsample file

--- downsample.py
+++ downsample.py
@@ -4,6 +4,9 @@ import numpy as np
 
 import glob
 
+import cv2
+
+
 def downsample():
 	count = 0
 	for img in glob.glob("test/sampled/*.jpg"):
@@ -19,3 +22,21 @@ def downsample():
     	
 
 downsample()
+
+def show_webcam(mirror=False):
+  cam = cv2.VideoCapture(0)
+	while True:
+		ret_val, img = cam.read()
+		if mirror: 
+			img = cv2.flip(img, 1)
+		cv2.imshow('my webcam', img)
+		if cv2.waitKey(1) == 27: 
+			break  # esc to quit
+	cv2.destroyAllWindows()
+
+def main():
+	show_webcam(mirror=True)
+
+if __name__ == '__main__':
+	main()
+
